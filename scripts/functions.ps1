0e6d845f26ea263a5db418137045fa87973ec049
Dennis Jay Dole <djdole@gmail.com>
2017-04-07 22:24:59 -0700

    Added unzip powershell function.

--- scripts/functions.ps1
+++ scripts/functions.ps1
@@ -1,3 +1,11 @@
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
 function Install-MSI
 {
   [CmdletBinding()]
