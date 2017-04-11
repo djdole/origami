0dbbcb17e127d3bc4ab7c8a5058a432e13c08d56
Dennis Jay Dole <djdole@gmail.com>
2017-04-10 17:29:30 -0700

    Updated Windows script to skip if a preexisting BLANK.box exists.

--- scripts/functions.ps1
+++ scripts/functions.ps1
@@ -6,33 +6,33 @@ function unzip
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
-function RmIfExists
+function Remove
 {
   [CmdletBinding()]
-    Param(
-      [parameter(mandatory=$true)]
-      [ValidateNotNullorEmpty()]
-      [string]$path
-    )
-	if([System.IO.File]::Exists($path))
-	{
-	  #rm "$path"
-	  Remove-Item -Path "$path" -Force
-	}
+  Param(
+    [parameter(mandatory=$true)]
+    [ValidateNotNullorEmpty()]
+    [string]$Path
+  )
+  if([System.IO.File]::Exists($Path))
+  {
+    #rm "$Path"
+    Remove-Item -Path "$Path" -Force
+  }
 }
 
 function Install-MSI
 {
   [CmdletBinding()]
-    Param(
-      [parameter(mandatory=$true,ValueFromPipeline=$true,ValueFromPipelinebyPropertyName=$true)]
-      [ValidateNotNullorEmpty()]
-      [string]$msi,
-      [parameter()]
-      [ValidateNotNullorEmpty()]
-      [string]$targetDir
-    )
-  if (!(Test-Path $msi))
+  Param(
+    [parameter(mandatory=$true,ValueFromPipeline=$true,ValueFromPipelinebyPropertyName=$true)]
+    [ValidateNotNullorEmpty()]
+    [string]$msi,
+    [parameter()]
+    [ValidateNotNullorEmpty()]
+    [string]$targetDir
+  )
+  if (-Not (Test-Path $msi))
   {
     throw "Path to the MSI File $($msi) is invalid. Please supply a valid MSI file"
   }
@@ -44,53 +44,55 @@ function Install-MSI
   )
   if ($targetDir)
   {
-    if (!(Test-Path $targetDir))
+    if (-Not (Test-Path $targetDir))
 	{
         throw "Path to the Installation Directory $($targetDir) is invalid. Please supply a valid installation directory"
     }
     $arguments += "INSTALLDIR=`"$targetDir`""
   }
-  Write-Host "Installing $msi ..."
+  Write-Host "Notice: Installing '$msi'..."
   $process = Start-Process -FilePath msiexec.exe -ArgumentList $arguments -Wait -PassThru
   if ($process.ExitCode -eq 0)
   {
-    Write-Host "Install successful."
+    Write-Host "Notice: Install successful."
   }
   ElseIf ($process.ExitCode -eq 3010)
   {
-    Write-Host "Install successful. REBOOT REQUIRED."
+    Write-Host "Notice: Install successful. REBOOT REQUIRED."
   }
   else
   {
-    Write-ERROR "Install exited with code $($process.ExitCode)."
+    Write-ERROR "Notice: Install exited with code $($process.ExitCode)."
   }
 }
 
 function Get-InstalledApps
 {
-    if ([IntPtr]::Size -eq 4) {
-        $regpath = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
-    }
-    else {
-        $regpath = @(
-            'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
-            'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
-        )
-    }
-    Get-ItemProperty $regpath | .{process{if($_.DisplayName -and $_.UninstallString) { $_ } }} | Select DisplayName, Publisher, InstallDate, DisplayVersion, UninstallString |Sort DisplayName
+  if ([IntPtr]::Size -eq 4)
+  {
+    $regpath = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+  }
+  else
+  {
+    $regpath = @(
+      'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+      'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+    )
+  }
+  Get-ItemProperty $regpath | .{process{if($_.DisplayName -and $_.UninstallString) { $_ } }} | Select DisplayName, Publisher, InstallDate, DisplayVersion, UninstallString |Sort DisplayName
 }
 
 function Download
 {
   [CmdletBinding()]
-    Param(
-      [parameter(mandatory=$true)]
-      [ValidateNotNullorEmpty()]
-      [string]$url,
-      [parameter(mandatory=$true)]
-      [ValidateNotNullorEmpty()]
-      [string]$saveto
-    )
+  Param(
+    [parameter(mandatory=$true)]
+    [ValidateNotNullorEmpty()]
+    [string]$url,
+    [parameter(mandatory=$true)]
+    [ValidateNotNullorEmpty()]
+    [string]$saveto
+  )
   $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
   [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
   wget -UseBasicParsing "$url" -OutFile "$saveto"
@@ -99,21 +101,50 @@ function Download
 function GetAndInstall-MSI
 {
   [CmdletBinding()]
-    Param(
-      [parameter(mandatory=$true)]
-      [ValidateNotNullorEmpty()]
-      [string]$name,
-      [parameter(mandatory=$true)]
-      [ValidateNotNullorEmpty()]
-      [string]$path,
-      [parameter(mandatory=$true)]
-      [ValidateNotNullorEmpty()]
-      [string]$url
-    )
+  Param(
+    [parameter(mandatory=$true)]
+    [ValidateNotNullorEmpty()]
+    [string]$name,
+    [parameter(mandatory=$true)]
+    [ValidateNotNullorEmpty()]
+    [string]$path,
+    [parameter(mandatory=$true)]
+    [ValidateNotNullorEmpty()]
+    [string]$url
+  )
   $result = Get-InstalledApps | where {$_.DisplayName -like $name}
   if ($result -eq $null)
   {
+	Write-Host "Notice: Downloading msi from '$url'..."
     Download -url "$url" -saveto "$path"
     "$path" | Install-MSI
   }
+}
+
+function BootstrapPacker
+{
+  [CmdletBinding()]
+  Param(
+    [parameter(mandatory=$true)]
+    [ValidateNotNullorEmpty()]
+    [string]$VMDir,
+    [parameter(mandatory=$true)]
+    [ValidateNotNullorEmpty()]
+    [string]$PackerUrl
+  )
+  
+  # Get packer from:
+  # https://releases.hashicorp.com/packer/1.0.0/
+  $PackerZip = $VMDir + "\packer.zip"
+  $PackerExe = $VMDir + "\packer.exe"
+  if(![System.IO.File]::Exists($PackerExe))
+  {
+    if(![System.IO.File]::Exists($PackerZip))
+    {
+	  Write-Host "Downloading zipped packer from '$PackerUrl'..."
+      Download -url "$PackerUrl" -saveto "$PackerZip"
+    }
+    unzip "$PackerZip" "$VMDir"
+  }
+  Remove -Path "$PackerZip"
 }
--- setup.ps1
+++ setup.ps1
@@ -9,43 +9,55 @@ $curdir = split-path -parent $MyInvocation.MyCommand.Definition
 $tempdir = $env:TEMP
 . "scripts\functions.ps1"
 
+$Name = "BLANK"
+$FileName = $Name + ".box"
+$RemoteBoxLoc = "\\morgan\DocuSign\Development\Connect"
+$RemoteFileName = $RemoteBoxLoc + "\" + $FileName
+
+$VMDir = $curdir + "\vm"
+$BoxFile = $VMDir + "\" + $FileName
+$BoxJson = $VMDir + "\" + $Name + ".json"
+
 # Install puppet
 GetAndInstall-MSI -name "*Puppet Agent*" -path "$tempdir\puppet-agent-x64-latest.msi" -url "https://downloads.puppetlabs.com/windows/puppet-agent-x64-latest.msi"
 
-#Install chrome, virtualbox, & vagrant
+#Install virtualbox, & vagrant
 puppet module install edestecd-software --version 1.1.0
-puppet apply "scripts\chrome.pp"
+#puppet apply "scripts\chrome.pp"
 puppet apply "scripts\virtualbox.pp"
 puppet apply "scripts\vagrant.pp"
 
-# Get packer
-#https://releases.hashicorp.com/packer/1.0.0/
-$VMDir = $curdir + "\vm"
-$PackerZip = $VMDir + "\packer.zip"
-$PackerExe = $VMDir + "\packer.exe"
-if(![System.IO.File]::Exists($PackerExe) -And ![System.IO.File]::Exists($PackerZip))
+# Check if an existing BOX file exists, if not create it.
+if(-Not (Test-Path "$RemoteFileName"))
 {
-  Download -url "https://releases.hashicorp.com/packer/1.0.0/packer_1.0.0_windows_amd64.zip" -saveto "$PackerZip"
-}
-if(![System.IO.File]::Exists($PackerExe))
-{
-  unzip "$PackerZip" "$VMDir"
-}
-RmIfExists -path "$PackerZip"
+  Write-Host "Notice: Existing '$FileName' VM image not found at '$RemoteBoxLoc'."
+  Write-Host "Notice: Preparing to create '$FileName'..."
 
-# Build Packer box.
-$name = "BLANK"
-$BoxFile = $VMDir + "\" + $name + ".box"
-$BoxFileFullPath = $VMDir + "\" + $BoxFile
-$BoxJson = $VMDir + "\" + $name + ".json"
-RmIfExists -path "$BoxFileFullPath"
+  # Bootstrap Packer
+  BootstrapPacker -VMDir "$VMDir" -PackerUrl "https://releases.hashicorp.com/packer/1.0.0/packer_1.0.0_windows_amd64.zip"
 
-pushd "$VMDir"
-./packer.exe build -only virtualbox-iso "$BoxJson"
-popd
-if([System.IO.File]::Exists($BoxFileFullPath))
-{
-  vagrant box remove "$name"
-  vagrant box add "$name" "$BoxFile"
+  # Build Packer box.
+  Remove -Path "$BoxFile"
+
+  pushd "$VMDir"
+    Write-Host "Notice: Creating '$FileName'..."
+    ./packer.exe build -only virtualbox-iso "$BoxJson"
+  popd
+  
+  if (-Not (Test-Path $targetDir))
+  {
+    New-Item -ItemType directory -Path "$RemoteBoxLoc" -Force
+  }
+  if (-Not (Test-Path $RemoteFileName))
+  {
+    Write-Host "Notice: Copying '$FileName' to '$RemoteBoxLoc'..."
+    Copy-Item -Path "$BoxFile" -Destination "$RemoteBoxLoc" -Force
+  }
 }
-RmIfExists -path "$BoxFileFullPath"
+
+if([System.IO.File]::Exists($BoxFile))
+{
+  vagrant box remove "$Name"
+  vagrant box add "$Name" "$BoxFile"
+}
+Remove -Path "$BoxFile"
